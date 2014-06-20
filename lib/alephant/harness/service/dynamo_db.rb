4afc5755e184a5adb4ec3979b77bd304f2355f42
Steven Jack <stevenmajack@gmail.com>
2014-06-20 16:01:16 +0100

    Adds delete functionality

--- lib/alephant/harness/service/dynamo_db.rb
+++ lib/alephant/harness/service/dynamo_db.rb
@@ -7,12 +7,23 @@ module Alephant
       module DynamoDB
 
         def self.client
-          ::AWS::DynamoDB::Client::V20120810.new
+          @@client ||= ::AWS::DynamoDB::Client::V20120810.new
         end
 
         def self.create(table_name, schema_name)
           schema = load_schema(schema_name).tap { |s| s[:table_name] = table_name }
           client.create_table schema
+        end
+
+        def self.remove(tables)
+          tables.each do |table_name|
+            begin
+              client.delete_table({ :table_name => table_name })
+            rescue Exception => e
+              puts "Rasing exception: #{e.message}"
+              #If table doesn't exist fail silently
+            end
+          end
         end
 
         def self.load_schema(schema_name)
