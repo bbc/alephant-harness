0c1b358c8f7b340943521f88db7aa9b1e386e923
Steven Jack <stevenmajack@gmail.com>
2014-06-20 16:01:06 +0100

    Adds delete method spec

--- spec/service_dynamo_db_spec.rb
+++ spec/service_dynamo_db_spec.rb
@@ -3,7 +3,6 @@ require 'spec_helper'
 describe Alephant::Harness::Service::DynamoDB do
 
   describe ".create" do
-
     it "creates a table based off a schema" do
 
       table_name = 'test_lookup'
@@ -16,7 +15,26 @@ describe Alephant::Harness::Service::DynamoDB do
       subject.create(table_name, schema_name)
 
     end
+  end
+
+  describe ".delete" do
+    let(:tables) { %w(foo bar) }
+
+    context "When tables exist" do
+      it "removes specified tables" do
+        expect_any_instance_of(AWS::DynamoDB::Client::V20120810).to receive(:delete_table).twice
+        subject.remove(tables)
+      end
+    end
+
+    context "When tables don't exist" do
+      it "Fails silently" do
+        expect_any_instance_of(AWS::DynamoDB::Client::V20120810).to receive(:delete_table).twice.and_raise(Exception)
+        expect { subject.remove(tables) }.to_not raise_error(Exception)
+      end
+    end
 
   end
+
 end
 
