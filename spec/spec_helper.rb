77ed0598591e2f95c7a199a40d0a4ceab9aa8f7a
Steven Jack <stevenmajack@gmail.com>
2014-06-20 15:31:01 +0100

    Adds dynamodb service and updates helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,4 +3,5 @@ $: << File.join(File.dirname(__FILE__), '..', 'lib')
 require 'pry'
 require 'alephant/harness'
 require 'alephant/harness/aws'
+require 'alephant/harness/service/dynamo_db'
 require 'aws-sdk'
