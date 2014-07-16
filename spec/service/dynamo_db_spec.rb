9fd936a1bd08b147e8da00f1e7923ab62b171a42
Steven Jack <stevenmajack@gmail.com>
2014-07-16 13:47:21 +0100

    Updates spec

--- spec/service/dynamo_db_spec.rb
+++ spec/service/dynamo_db_spec.rb
@@ -7,12 +7,13 @@ describe Alephant::Harness::Service::DynamoDB do
 
       table_name = 'test_lookup'
       schema_name = 'lookup'
+      schema = subject.load_schema(schema_name)
 
       expected_schema = YAML::load_file(File.join(File.dirname(__FILE__), *[%w'..' * 2], 'schema', "#{schema_name}.yaml"))
       expected_schema[:table_name] = table_name
 
       expect_any_instance_of(AWS::DynamoDB::Client::V20120810).to receive(:create_table).with(expected_schema)
-      subject.create(table_name, schema_name)
+      subject.create(table_name, schema)
 
     end
   end
@@ -23,14 +24,16 @@ describe Alephant::Harness::Service::DynamoDB do
     context "When tables exist" do
       it "removes specified tables" do
         expect_any_instance_of(AWS::DynamoDB::Client::V20120810).to receive(:delete_table).twice
-        subject.remove(tables)
+        tables.each do |table|
+          subject.remove(table)
+        end
       end
     end
 
     context "When tables don't exist" do
       it "Fails silently" do
-        expect_any_instance_of(AWS::DynamoDB::Client::V20120810).to receive(:delete_table).twice.and_raise(Exception)
-        expect { subject.remove(tables) }.to_not raise_error(Exception)
+        expect_any_instance_of(AWS::DynamoDB::Client::V20120810).to receive(:delete_table).and_raise(Exception)
+        expect { subject.remove('blah') }.to_not raise_error(Exception)
       end
     end
 
